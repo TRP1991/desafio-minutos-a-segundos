559b099fe31002f7c28e4103423070aec5bf556e
Tamara Rubilar <ta.rubilar@gmail.com>
2020-07-03 23:55:27 -0400

    start solution program transforming seconds to minutes

---
+++ s_to_m.rb
@@ -0,0 +1,14 @@
+=begin 
+
+Se tiene un arreglo con la cantidad de segundos que demoraron algunos procesos y se necesita un
+método para trasformar todos los datos a minutos (las fracciones de minuto serán ignoradas). Para
+realizar este programa se debe crear el archivo s_to_m.rb
+
+El método debe llamarse to_minutes . Debe recibir el arreglo con los tiempos en segundos y
+devolverlo con los tiempos en minutos.
+
+seconds = [100, 50, 1000, 5000, 1000, 500]
+
+=end 
+
+def 
