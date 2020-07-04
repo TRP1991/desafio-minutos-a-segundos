77b49a15d939625fd1a804b9c174b2e14230d088
Tamara Rubilar <ta.rubilar@gmail.com>
2020-07-03 23:59:09 -0400

    the array receives the times in seconds

--- s_to_m.rb
+++ s_to_m.rb
@@ -11,4 +11,13 @@ seconds = [100, 50, 1000, 5000, 1000, 500]
 
 =end 
 
-def 
+def to_minutes(array)
+    n = array.count
+    result []
+    n.times do |i|
+        result.push array[i] / 60
+    end
+    result
+end
+
+seconds = [100, 50, 1000, 5000, 1000, 500]
